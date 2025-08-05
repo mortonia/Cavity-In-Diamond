9915867c87a4fd88bae483fe98e4995cb3e058fc
Iain Morton <139282483+mortonia@users.noreply.github.com>
2025-08-05 10:49:40 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,26 @@
-# config.py
+import numpy as np
+from ase.io import read
+from molecule_utils import compute_cavity_radii
 
-# Toggle between atomic and molecular crystals
 USE_MOLECULAR_CRYSTAL = False
 MOLECULAR_CRYSTAL_FILE = 'ice_unitcell.xyz'
+MOLECULE_PATH = 'molecules_xyz/propiolic.xyz'
+molecule = read(MOLECULE_PATH)
 
-# For atomic crystal fallback
-ELEMENT = 'C'
-LATTICE_CONSTANT = 3.57  # Å
-
-REPEAT = (7, 7, 7)
-CAVITY_BUFFER = 2.0
+ELEMENT = 'Ar'
+LATTICE_CONSTANT = 5.30
+REPEAT = (10, 10, 10)
+CAVITY_BUFFER = 3.0
+CAVITY_RADII = compute_cavity_radii(molecule, CAVITY_BUFFER)
 DISPLACEMENT_THRESHOLD = 0.1
 
-# Lennard-Jones parameters per element (eV, Å)
 LJ_PARAMS = {
-    'C': {'epsilon': 0.00284, 'sigma': 3.40},
-    'H': {'epsilon': 0.00236, 'sigma': 2.96},
-    'O': {'epsilon': 0.00674, 'sigma': 3.00},
-    'N': {'epsilon': 0.00739, 'sigma': 3.20},
-    'Ar': {'epsilon': 0.0103,  'sigma': 3.40},
+    'C': {'epsilon':0.00284,'sigma':3.40},
+    'H': {'epsilon':0.00236,'sigma':2.96},
+    'O': {'epsilon':0.00674,'sigma':3.00},
+    'N': {'epsilon':0.00739,'sigma':3.20},
+    'Ar':{'epsilon':0.0103,'sigma':3.40},
+    'Ne':{'epsilon':0.0031,'sigma':2.75},
 }
 
+OUTPUT_DIR = 'outputs'
